4ba9e08279bfcb82679f170c58e31151caf11762
W20MC <157727813+W20MC@users.noreply.github.com>
2024-04-29 17:02:04 +0200

    make proper speedhack

--- src/Hacks/SpeedHack.cpp
+++ src/Hacks/SpeedHack.cpp
@@ -1,14 +1,44 @@
 #include "Includes/Hacks.hpp"
 #include <Geode/modify/CCScheduler.hpp>
 
+FMOD::ChannelGroup* masterGroup;
+
 void Modules::speedHack() {
-	ImGui::SetNextWindowSize(ImVec2(202, 99), ImGuiCond_Once);
+	ImGui::SetNextWindowSize(ImVec2(202, 110), ImGuiCond_Once);
 	ImGui::SetNextWindowPos(ImVec2(215, 60), ImGuiCond_Once);
 
-	ImGui::Begin("Speedhack (WIP)");
+	ImGui::Begin("Speedhack");
 
 	UltraHack::get().createCheckbox("Enable", "speedhack");
+	UltraHack::get().createCheckbox("Speedhack Music", "speedhack-music");
 	UltraHack::get().createInputFloat("Value", "speedhack-value");
 
 	ImGui::End();
 }
+
+class $modify(CCScheduler) {
+	virtual void update(float dt) {
+		if (!masterGroup) {
+			FMODAudioEngine::sharedEngine()->m_system->getMasterChannelGroup(&masterGroup);
+		}
+
+		if (UltraHack::get().idAccessor<bool>("get", "speedhack")) {
+			if (UltraHack::get().idAccessor<float>("get", "speedhack-value") <= 0.0f) {
+				UltraHack::get().idAccessor<float>("set", "speedhack-value", 1.0f);
+			}
+			else {
+				dt = dt * UltraHack::get().idAccessor<float>("get", "speedhack-value");
+				if (UltraHack::get().idAccessor<bool>("get", "speedhack-music")) {
+					masterGroup->setPitch(UltraHack::get().idAccessor<float>("get", "speedhack-value"));
+				}
+				else {
+					masterGroup->setPitch(1);
+				}
+			}
+		}
+		else {
+			masterGroup->setPitch(1);
+		}
+		CCScheduler::update(dt);
+	}
+};
