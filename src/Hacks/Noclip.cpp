a2b10632afd020d228c0d40235c5ac55e5beb5fb
W20MC <157727813+W20MC@users.noreply.github.com>
2024-05-01 08:15:22 +0200

    removing shitty code part 2

--- src/Hacks/Noclip.cpp
+++ src/Hacks/Noclip.cpp
@@ -5,7 +5,7 @@ using namespace geode::prelude;
 
 class $modify(PlayLayer) {
     void destroyPlayer(PlayerObject* p0, GameObject* p1) {
-        if (UltraHack::get().idAccessor<bool>("get", "noclip") == true);
+        if (Mod::get()->getSavedValue<bool>("noclip") == true);
         else {
             PlayLayer::destroyPlayer(p0, p1);
         }
